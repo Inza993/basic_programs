13e26a11b38ea8f729087e9f6f5d26a576eaa414
arun-dh <arun.s@dedalus.eu>
2026-03-11 11:04:18 +0530

    Collections LinkedList

--- src/collection/DemoArrayList.java
+++ src/collection/DemoArrayList.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class DemoList {
+public class DemoArrayList {
 
     public static void main(String[] args) {
 
@@ -48,6 +48,44 @@ public class DemoList {
 
         laptops.get(0).setFingerUnlockAvailable(true);
         System.out.println(laptops.get(0));
+
+        List<String> ls = new ArrayList<>();
+        ls.add("a");
+        ls.add("b");
+        System.out.println(ls);
+
+        List<Integer> ls1 = new ArrayList<>();
+        int no = 5;
+        Integer i = 10;
+        Integer.parseInt("10");
+
+        Integer a = 100; // Autoboxing, uses Integer.valueOf(100)
+        Integer b = 100; // Autoboxing, uses the same cached object
+        System.out.println(a == b); // true
+
+        Integer c = 200; // Value outside the default cache range
+        Integer d = 200; // A new object is created
+        System.out.println(c == d); // false
+
+        int c1 = c; // Auto un-boxing
+        System.out.println(c1);
+
+        ls1.add(a);
+        ls1.add(no);
+
+        // Array vs ArrayList
+        // Array: fixed size
+        // Arraylist: dynamic size can shrink or grow
+
+        System.out.println("Before add(1, 11): "+ls1);
+        ls1.add(1, 11);
+        System.out.println("After add(1, 11): "+ls1);
+
+        System.out.println("Before remove(0): "+ls1);
+        ls1.remove(0);
+        System.out.println("After remove(0): "+ls1);
+
+
     }
 
     static void of(long... elements) {
